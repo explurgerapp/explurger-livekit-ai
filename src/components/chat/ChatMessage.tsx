452b73a792b74fa01985eb39ccce2dd8160361dc
explurger-admin <141579158+explurger-admin@users.noreply.github.com>
2025-06-15 01:19:36 +0530

    Update ChatMessage.tsx

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -13,23 +13,27 @@ export const ChatMessage = ({
   isSelf,
   hideName,
 }: ChatMessageProps) => {
+  const bubbleAlignment = isSelf ? "self-end" : "self-start";
+  const bubbleColor = isSelf ? `bg-lime-600` : "bg-gray-200";
+  const textColor = isSelf ? "text-white" : "text-gray-900";
+
   return (
-    <div className={`flex flex-col gap-1 ${hideName ? "pt-0" : "pt-6"}`}>
+    <div
+      className={`flex flex-col gap-1 w-full my-1 ${
+        hideName ? "pt-0" : "pt-2"
+      }`}
+    >
       {!hideName && (
         <div
-          className={`text-${
-            isSelf ? "gray-700" : accentColor + "-800 text-ts-" + accentColor
-          } uppercase text-xs`}
+          className={`text-xs text-gray-500 ${
+            isSelf ? "text-right" : "text-left"
+          } px-2`}
         >
           {name}
         </div>
       )}
       <div
-        className={`pr-4 text-${
-          isSelf ? "gray-300" : accentColor + "-500"
-        } text-sm ${
-          isSelf ? "" : "drop-shadow-" + accentColor
-        } whitespace-pre-line`}
+        className={`px-4 py-2 rounded-lg max-w-[80%] whitespace-pre-line ${bubbleAlignment} ${bubbleColor} ${textColor}`}
       >
         {message}
       </div>
